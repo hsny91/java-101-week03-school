c4442759a09abc951fb5c2f9a4c87cf592a03a35
Husniye <husniyesakmak.91@gmail.com>
2020-12-12 12:56:25 +0100

    project modified

--- src/ch/hicoders/jam101/week03/School.java
+++ src/ch/hicoders/jam101/week03/School.java
@@ -4,11 +4,20 @@ import java.util.*;
 
 public class School {
     public static void main(String[] args) {
-        System.out.println("deneme");
         ArrayList<Student> studentList = School.loadStudentsData();
         School.renderArray(studentList);
         float averageList[]= School.averageStudentNote(studentList);
         School.renderAverageNote(averageList);
+        //School.findMaxMinMathNote(studentList);
+        ArrayList<Student> successMathStudentsList = School.successMathStudentsData(studentList,averageList[0]);
+        School.renderArray(successMathStudentsList);
+        ArrayList<Student> unsuccessMathStudentsList = School.unsuccessMathStudentsData(studentList,averageList[0]);
+        School.renderArray(unsuccessMathStudentsList);
+        ArrayList<Student> successGermanStudentsList = School.successGermanStudentsData(studentList,averageList[1]);
+        School.renderArray(successGermanStudentsList);
+        ArrayList<Student> unsuccessGermanStudentsList = School.unsuccessGermanStudentsData(studentList,averageList[1]);
+        School.renderArray(unsuccessGermanStudentsList);
+
 
     }
 // ogrenci bilgilerini olusturan ve diziye ekleyen method.
@@ -51,4 +60,71 @@ public class School {
         System.out.println("Average German Grades: "+pArray[1]);
 
     }
+    // en yuksek ve en dusuk alan ogrencileri bulan method
+    public static void  findMaxMinMathNote(ArrayList<Student> pStudentList){
+        int maxNote=pStudentList.get(0).mathNote;
+        int minNote=pStudentList.get(0).mathNote;
+        for(Student student:pStudentList)
+        {
+           if(student.mathNote>maxNote){
+               maxNote=student.mathNote;
+           }
+           if(student.mathNote<minNote){
+               minNote=student.mathNote;
+           }
+        }
+        System.out.println(maxNote);
+        System.out.println(minNote);
+    }
+    //Matamatikten gecen ogrenciler
+    public static ArrayList<Student> successMathStudentsData(ArrayList<Student> pStudentList, float pAverage){
+        ArrayList<Student>successStudentsList = new ArrayList<Student>();
+        for(Student student:pStudentList){
+           if(student.mathNote>=pAverage){
+               successStudentsList.add(student);
+           }
+        }
+        System.out.println("matematik dersinde basarili olan ogrenciler: ");
+
+        return successStudentsList;
+    }
+    //Matamatikten kalan ogrenciler
+    public static ArrayList<Student> unsuccessMathStudentsData(ArrayList<Student> pStudentList, float pAverage){
+        ArrayList<Student>unsuccessStudentsList = new ArrayList<Student>();
+        for(Student student:pStudentList){
+            if(student.mathNote<pAverage){
+                unsuccessStudentsList.add(student);
+            }
+        }
+        System.out.println("matematik dersinde kalan ogrenciler: ");
+
+        return unsuccessStudentsList;
+    }
+    //Almancadan gecen ogrenciler
+
+    public static ArrayList<Student> successGermanStudentsData(ArrayList<Student> pStudentList, float pAverage){
+        ArrayList<Student> successGermanStudentList = new ArrayList<Student>();
+        for(Student student:pStudentList){
+            if(student.germanNote>=pAverage){
+                successGermanStudentList.add(student);
+            }
+        }
+        System.out.println("Almanca dersinde basarili olan ogrenciler: ");
+
+        return successGermanStudentList;
+    }
+    //Almancadan kalan ogrenciler
+
+    public static ArrayList<Student> unsuccessGermanStudentsData(ArrayList<Student> pStudentList, float pAverage){
+        ArrayList<Student> unsuccessGermanStudentList = new ArrayList<Student>();
+        for(Student student:pStudentList){
+            if(student.germanNote<pAverage){
+                unsuccessGermanStudentList.add(student);
+            }
+        }
+        System.out.println("Almanca dersinden kalan ogrenciler: ");
+
+        return unsuccessGermanStudentList;
+    }
+
 }
