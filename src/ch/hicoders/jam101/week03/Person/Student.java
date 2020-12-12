bde2a10321e142aad158af6c3baf0b3196f25535
Husniye <husniyesakmak.91@gmail.com>
2020-12-12 09:33:27 +0100

    first commit

---
+++ src/ch/hicoders/jam101/week03/Person/Student.java
@@ -0,0 +1,14 @@
+package ch.hicoders.jam101.week03.Person;
+
+public class Student {
+    public String studentName;
+    public int mathNote;
+    public int germanNote;
+
+    public Student(String pStudentName,int pMathNote,int pGermanNote){
+        this.studentName=pStudentName;
+        this.mathNote=pMathNote;
+        this.germanNote=pGermanNote;
+
+    }
+}
---
+++ src/ch/hicoders/jam101/week03/School.java
@@ -0,0 +1,7 @@
+package ch.hicoders.jam101.week03;
+
+public class School {
+    public static void main(String[] args){
+       System.out.println("deneme");
+    }
+}
